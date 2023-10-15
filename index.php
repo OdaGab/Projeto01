f71bffa317395a88064f14a165a685c46e2d35fb
Odair <odavita32@gmail.com>
2023-10-15 20:59:20 -0300

    linha de corte

--- index.php
+++ index.php
@@ -6,12 +6,17 @@ require('vendor\fpdf\fpdf\original\rounded_rect2.php');
 $pdf = new PDF('P', 'cm', 'A4');
 $pdf->AddPage();
 
-
 $pdf->SetFillColor(400);
 
 $pdf->RoundedRect( 0.1, 0.1,   16.8,    1, 0.1, '1234');
 $pdf->RoundedRect( 0.1, 1.1,  3.5, 0.80, 0.1, '1234');
 $pdf->RoundedRect( 3.6, 1.1, 13.3, 0.80, 0.1, '1234');
 $pdf->RoundedRect(16.9, 0.1,  4, 1.80, 0.1, '1234');
+
+
+$pdf->SetFont('Arial');
+
+$pdf->Text(0.1, 1.92, '_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _');
+
 
 $pdf->Output('F', 'C:/xampp/htdocs/Projeto01/download/report2.pdf');
